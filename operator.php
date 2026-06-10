6648fc17254e144c5ec88c9ead96884c9c51d511
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-06-10 16:55:57 +0530

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo "addition: ".$x + $y;
+echo "Subtraction: ".$x - $y;
+echo "Multiplication: ".$x * $y;
+echo "division: ".$x / $y;
+
+?>  
+
+</body>
+</html>
