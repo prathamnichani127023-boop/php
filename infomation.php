2b7f8bf4d2ffe4a544de69113f0e02be0314a1db
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-06-12 12:20:59 +0530

    Add files via upload

---
+++ infomation.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Data Form</title>
+</head>
+<body>
+
+<h1> User Data </h1>
+
+<form>
+    <fieldset>
+        <legend>User Information</legend>
+
+        Enter Name :
+        <input type="text"><br><br>
+
+        Enter Enrollment Number :
+        <input type="text"><br><br>
+
+        Enter GR Number :
+        <input type="text"><br><br>
+
+        Enter Sem :
+        <input type="text"><br><br>
+
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+    </fieldset>
+</form>
+
+</body>
+</html>
