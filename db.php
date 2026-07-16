d045de6f44804a0d39460852adf7a979b25bc165
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-16 18:09:08 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
+
 $host = "localhost";
 $user = "root";
-$pass = "";
-$dbname = "login";
+$password = "";
+$database = "singup1";
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn)
 {
-    die("Database Connection Failed: " . mysqli_connect_error());
+    die("Database Connection Failed : " . mysqli_connect_error());
 }
+
 ?>
