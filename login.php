04b2e0414a6ec69519d6a55c3d2225aa33917ef6
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-29 10:59:18 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,70 +1,23 @@
-<?php
-include("db.php");
-
-$message = "";
-
-if(isset($_POST['login']))
-{
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-
-    if(mysqli_num_rows($result) > 0)
-    {
-        $message = "Login Successful";
-    }
-    else
-    {
-        $message = "Invalid Username or Password";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
+    <title>Login Form</title>
 </head>
 <body>
 
-<center>
-
 <h2>Login Form</h2>
 
-<?php
-if($message != "")
-{
-    echo "<h3>$message</h3>";
-}
-?>
+<form action="check_login.php" method="post">
 
-<form method="post">
+Username:
+<input type="text" name="username" required><br><br>
 
-    <table border="1" cellpadding="10">
+Password:
+<input type="password" name="password" required><br><br>
 
-        <tr>
-            <td>Username</td>
-            <td><input type="text" name="username" required></td>
-        </tr>
-
-        <tr>
-            <td>Password</td>
-            <td><input type="password" name="password" required></td>
-        </tr>
-
-        <tr>
-            <td colspan="2" align="center">
-                <input type="submit" name="login" value="Login">
-            </td>
-        </tr>
-
-    </table>
+<input type="submit" value="Login">
 
 </form>
-
-</center>
 
 </body>
 </html>
