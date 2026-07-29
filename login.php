0d4fcc9029d0365ea1a9bd69ec8c563b3c6357cf
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-29 10:59:54 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+if(isset($_POST['login']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Store cookies for 1 month
+    setcookie("username", $username, time() + (30 * 24 * 60 * 60));
+    setcookie("password", $password, time() + (30 * 24 * 60 * 60));
+
+    echo "Login Successful.<br>";
+    echo "<a href='read_cookie.php'>Go to Next Page</a>";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,16 +22,18 @@
 
 <h2>Login Form</h2>
 
-<form action="check_login.php" method="post">
+<form method="post">
+    Username:
+    <input type="text" name="username"
+    value="<?php if(isset($_COOKIE['username'])) echo $_COOKIE['username']; ?>">
+    <br><br>
 
-Username:
-<input type="text" name="username" required><br><br>
+    Password:
+    <input type="password" name="password"
+    value="<?php if(isset($_COOKIE['password'])) echo $_COOKIE['password']; ?>">
+    <br><br>
 
-Password:
-<input type="password" name="password" required><br><br>
-
-<input type="submit" value="Login">
-
+    <input type="submit" name="login" value="Login">
 </form>
 
 </body>
